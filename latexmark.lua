9b3334bc2bf74bf81f79bcddec04168d81cb6964
Michal Hoftich <michal.h21@gmail.com>
2014-11-01 13:59:56 +0100

    added package options for extensions and templates

--- latexmark.lua
+++ latexmark.lua
@@ -2,7 +2,14 @@ local latexmark = {}
 local lunamark = require("lunamark")
 local writer = lunamark.writer.latex.new()
 local util = lunamark.util
+local cosmo = require "cosmo"
+
+-- fixes for lua 5.1 functions used in cosmo
+unpack = unpack or table.unpack
+loadstring = loadstring or load
+
 writer.string = function(s) return s end
+
 writer.definitionlist= function(items)
     local buffer = {}
     for _,item in ipairs(items) do
@@ -13,10 +20,20 @@ writer.definitionlist= function(items)
     return {"\\begin{description}\n",contents,"\n\\end{description}"}
  end
 latexmark.init = function(extensions)
-   local extensions = extensions or { smart = true, definition_lists=true}
+   local extensions = #extensions > 0 and extensions or { smart = true, definition_lists=true}
    latexmark.parser = lunamark.reader.markdown.new(writer, extensions)
 end
 
+latexmark.template = function(tpl)
+  if string.len(tpl) == 0 then return false,"No template" end
+  local f = io.open(kpse.find_file(tpl),"r")
+  if not f then 
+    print("latexmark: cannot load template file: "..tpl)
+  else
+    writer.template = f:read("*all")
+    f:close()
+  end
+end
 
 latexmark.end_env = "%s*\\end{latexmark}"
 local buffer = {}
@@ -31,13 +48,19 @@ latexmark.callback = function(buf)
 end
 
 latexmark.process = function()
-  local result = latexmark.parser(table.concat(buffer,"\n"))
+  local body, metadata = latexmark.parser(table.concat(buffer,"\n"))
+  local template = writer.template or "$body"
+  -- erase current buffer
   buffer = {}
+  metadata = metadata or {}
+  metadata.body = body
+  result = cosmo.fill(template, metadata)
   local lines = string.explode(result,"\n")
   for _, line in ipairs(lines) do
     print(line)
-    tex.print(line)
+    -- tex.print(line)
   end
+  tex.print(lines)
   return lines
 end
 
