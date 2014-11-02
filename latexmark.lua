543f7e6f2f04ea0cde604295730efd683569b0a2
Michal Hoftich <michal.h21@gmail.com>
2014-11-02 12:18:27 +0100

    Fixed template loading

--- latexmark.lua
+++ latexmark.lua
@@ -9,6 +9,7 @@ unpack = unpack or table.unpack
 loadstring = loadstring or load
 
 writer.string = function(s) return s end
+latexmark.template = nil 
 
 writer.definitionlist= function(items)
     local buffer = {}
@@ -20,17 +21,19 @@ writer.definitionlist= function(items)
     return {"\\begin{description}\n",contents,"\n\\end{description}"}
  end
 latexmark.init = function(extensions)
-   local extensions = #extensions > 0 and extensions or { smart = true, definition_lists=true}
+   local extensions = next(extensions) ~= nil and extensions or { smart = true, definition_lists=true}
    latexmark.parser = lunamark.reader.markdown.new(writer, extensions)
 end
 
-latexmark.template = function(tpl)
-  if string.len(tpl) == 0 then return false,"No template" end
+latexmark.save_template = function(tpl)
+  if string.len(tpl) == 0 then 
+    return false,"No template" 
+  end
   local f = io.open(kpse.find_file(tpl),"r")
   if not f then 
     print("latexmark: cannot load template file: "..tpl)
   else
-    writer.template = f:read("*all")
+    latexmark.template = f:read("*all")
     f:close()
   end
 end
@@ -49,10 +52,17 @@ end
 
 latexmark.process = function()
   local body, metadata = latexmark.parser(table.concat(buffer,"\n"))
-  local template = writer.template or "$body"
+  local template = latexmark.template or "$body"
+  print("template", template)
   -- erase current buffer
   buffer = {}
   metadata = metadata or {}
+  for k,v in pairs(metadata) do
+    print("meta",k,v)
+    for i, j in pairs(v) do
+      print(i,j)
+    end
+  end
   metadata.body = body
   result = cosmo.fill(template, metadata)
   local lines = string.explode(result,"\n")
